0aa93d08fdc3bc6853cd19d050c7f970616e3cf2
noumansalik <37512775+noumansalik@users.noreply.github.com>
2025-05-01 16:33:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import dash
 from dash import html
 
 app = dash.Dash(__name__)
-app.layout = html.Div(\" Hello from Dash!\")
+app.layout = html.Div(" Hello from Dash!\")
 
 application = app  #  this must be at the top level
 
-if __name__ == \"__main__\":
+if __name__ == "__main__\":
     app.run(debug=True)
